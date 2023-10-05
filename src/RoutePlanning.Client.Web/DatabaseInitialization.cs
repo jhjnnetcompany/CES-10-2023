4b4a1fe62b9aed6c722b6980bdedc3b80001262b
Hermansson99 <146936147+Hermansson99@users.noreply.github.com>
2023-10-05 10:12:19 +0200

    Added locations and air connections. Also added Dan as a user (#1)

--- src/RoutePlanning.Client.Web/DatabaseInitialization.cs
+++ src/RoutePlanning.Client.Web/DatabaseInitialization.cs
@@ -26,26 +26,129 @@ public static class DatabaseInitialization
 
     private static async Task SeedLocationsAndRoutes(RoutePlanningDatabaseContext context)
     {
-        var berlin = new Location("Berlin");
-        await context.AddAsync(berlin);
+        var cairo = new Location("Cairo");
+        await context.AddAsync(cairo);
 
-        var copenhagen = new Location("Copenhagen");
-        await context.AddAsync(copenhagen);
+        var tripoli = new Location("Tripoli");
+        await context.AddAsync(tripoli);
 
-        var paris = new Location("Paris");
-        await context.AddAsync(paris);
+        var tunis = new Location("Tunis");
+        await context.AddAsync(tunis);
 
-        var warsaw = new Location("Warsaw");
-        await context.AddAsync(warsaw);
+        var marrakesh = new Location("Marrakesh");
+        await context.AddAsync(marrakesh);
 
-        CreateTwoWayConnection(berlin, warsaw, 573);
-        CreateTwoWayConnection(berlin, copenhagen, 763);
-        CreateTwoWayConnection(berlin, paris, 1054);
-        CreateTwoWayConnection(copenhagen, paris, 1362);
+        var canaryIslands = new Location("Canary island");
+        await context.AddAsync(canaryIslands);
+
+        var tanger = new Location("Tanger");
+        await context.AddAsync(tanger);
+
+        var sahara = new Location("Sahara");
+        await context.AddAsync(sahara);
+
+        var omdurman = new Location("Omdurman");
+        await context.AddAsync(omdurman);
+
+        var suakin = new Location("Suakin");
+        await context.AddAsync(suakin);
+
+        var addisAbeba = new Location("Addis Abeba");
+        await context.AddAsync(addisAbeba);
+
+        var kapSuardafui = new Location("Kap Suardafui");
+        await context.AddAsync(kapSuardafui);
+
+        var wadai = new Location("Wadai");
+        await context.AddAsync(wadai);
+
+        var sierraLeone = new Location("Sierra Leone");
+        await context.AddAsync(sierraLeone);
+
+        var dakar = new Location("Dakar");
+        await context.AddAsync(dakar);
+
+        var darfur = new Location("Darfur");
+        await context.AddAsync(darfur);
+
+        var timbuktu = new Location("Timbuktu");
+        await context.AddAsync(timbuktu);
+
+        var goldCoast = new Location("Gold Coast");
+        await context.AddAsync(goldCoast);
+
+        var slaveCoast = new Location("Slave Coast");
+        await context.AddAsync(slaveCoast);
+
+        var bahrelGhazal = new Location("Bahrel Ghazal");
+        await context.AddAsync(bahrelGhazal);
+
+        var lakeVictoria = new Location("Lake Victoria");
+        await context.AddAsync(lakeVictoria);
+
+        var zanzibar = new Location("Zanzibar");
+        await context.AddAsync(zanzibar);
+
+        var congo = new Location("Congo");
+        await context.AddAsync(congo);
+
+        var kabalo = new Location("Kabalo");
+        await context.AddAsync(kabalo);
+
+        var luanda = new Location("Luanda");
+        await context.AddAsync(luanda);
+
+        var victoriaFalls = new Location("Victoria Falls");
+        await context.AddAsync(victoriaFalls);
+
+        var mocambique = new Location("Mocambique");
+        await context.AddAsync(mocambique);
+
+        var stHelena = new Location("St. Helena");
+        await context.AddAsync(stHelena);
+
+        var whaleBay = new Location("Whale Bay");
+        await context.AddAsync(whaleBay);
+
+        var dragonMountain = new Location("Dragon Mountain");
+        await context.AddAsync(dragonMountain);
+
+        var capeTown = new Location("Cape Town");
+        await context.AddAsync(capeTown);
+
+        var amatave = new Location("Amatave");
+        await context.AddAsync(amatave);
+
+        var stMarie = new Location("St. Marie");
+        await context.AddAsync(stMarie);
+
+        CreateTwoWayConnection(tripoli, tanger, 8);
+        CreateTwoWayConnection(tanger, marrakesh, 8);
+        CreateTwoWayConnection(marrakesh, sierraLeone, 8);
+        CreateTwoWayConnection(marrakesh, goldCoast, 8);
+        CreateTwoWayConnection(tripoli, goldCoast, 8);
+        CreateTwoWayConnection(sierraLeone, stHelena, 8);
+        CreateTwoWayConnection(stHelena, capeTown, 8);
+        CreateTwoWayConnection(capeTown, whaleBay, 8);
+        CreateTwoWayConnection(capeTown, dragonMountain, 8);
+        CreateTwoWayConnection(capeTown, amatave, 8);
+        CreateTwoWayConnection(capeTown, stMarie, 8);
+        CreateTwoWayConnection(dragonMountain, lakeVictoria, 8);
+        CreateTwoWayConnection(lakeVictoria, kapSuardafui, 8);
+        CreateTwoWayConnection(suakin, darfur, 8);
+        CreateTwoWayConnection(suakin, cairo, 8);
+        CreateTwoWayConnection(darfur, kabalo, 8);
+        CreateTwoWayConnection(tripoli, darfur, 8);
+        CreateTwoWayConnection(goldCoast, luanda, 8);
+        CreateTwoWayConnection(goldCoast, whaleBay, 8);
+        CreateTwoWayConnection(amatave, kapSuardafui, 8);
     }
 
     private static async Task SeedUsers(RoutePlanningDatabaseContext context)
     {
+        var dan = new User("dan", User.ComputePasswordHash("dan123!"));
+        await context.AddAsync(dan);
+
         var alice = new User("alice", User.ComputePasswordHash("alice123!"));
         await context.AddAsync(alice);
 
