7750ec4473ceb247e41ada36c1fd4a4eaa66b327
Hermansson99 <146936147+Hermansson99@users.noreply.github.com>
2023-10-06 11:31:43 +0200

    price update (#16)

--- src/RoutePlanning.Domain/ParcelPrice.cs
+++ src/RoutePlanning.Domain/ParcelPrice.cs
@@ -4,9 +4,9 @@ namespace RoutePlanning.Domain.Locations;
 
 public sealed class ParcelPrice : AggregateRoot<ParcelPrice>
 {
-    public ParcelSize? ParcelSize { get; set; }
+    public ParcelSize ParcelSize { get; set; } = default!;
 
-    public ParcelWeight? ParcelWeight { get; set; }
+    public ParcelWeight ParcelWeight { get; set; } = default!;
 
     public double priceInDollars { get; set; }
 }
--- src/RoutePlanning.Domain/ParcelSize.cs
+++ src/RoutePlanning.Domain/ParcelSize.cs
@@ -4,7 +4,7 @@ using Netcompany.Net.DomainDrivenDesign.Models;
 namespace RoutePlanning.Domain.Locations;
 
 [DebuggerDisplay("{Name}")]
-public sealed class ParcelSize : Entity<ParcelSize>
+public sealed class ParcelSize : AggregateRoot<ParcelSize>
 {
     public string Name { get; set; } = default!;
     public double MaxHeight { get; set; }
